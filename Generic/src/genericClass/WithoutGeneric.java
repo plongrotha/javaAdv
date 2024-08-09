237432042558816a23a9e5e712e8c5e3b45d0b60
plong rotha <Mrr.rothabetta31@gmail.com>
2024-08-09 09:50:08 +0700

    delete line that is Integer

--- Generic/src/genericClass/WithoutGeneric.java
+++ Generic/src/genericClass/WithoutGeneric.java
@@ -14,7 +14,6 @@ public class WithoutGeneric {
   public static void main(String[] args) {
     WithoutGeneric type = new WithoutGeneric();
     type.setValue("leak");
-    type.setValue(20); // this line value is Integer can not cast to String please fix it
     String st = (String) type.getValue();
     System.out.println(st);
   }
