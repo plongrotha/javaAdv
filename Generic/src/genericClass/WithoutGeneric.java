cde4bc2285f3e1ae48d9b2eb3214a15d29917549
plong rotha <Mrr.rothabetta31@gmail.com>
2024-08-09 09:41:51 +0700

    write and run

--- Generic/src/genericClass/WithoutGeneric.java
+++ Generic/src/genericClass/WithoutGeneric.java
@@ -1,5 +1,22 @@
 package genericClass;
 
 public class WithoutGeneric {
+  private Object value;
+
+  public Object getValue() {
+    return value;
+  }
+
+  public void setValue(Object value) {
+    this.value = value;
+  }
+
+  public static void main(String[] args) {
+    WithoutGeneric type = new WithoutGeneric();
+    type.setValue("leak");
+    String st = (String) type.getValue();
+    System.out.println(st);
+
+  }
 
 }
