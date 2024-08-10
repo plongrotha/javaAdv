2b3a7145cb225adfdb92441ebcaa03c9d4057753
plong rotha <Mrr.rothabetta31@gmail.com>
2024-08-10 10:08:32 +0700

    added of Number

--- Generic/src/genericBoundedTypeParameter/GenericBoundedTypeParameter.java
+++ Generic/src/genericBoundedTypeParameter/GenericBoundedTypeParameter.java
@@ -20,5 +20,9 @@ public class GenericBoundedTypeParameter {
     List<Long> num = List.of(29l, 44l, 562l, 678l);
     double total2 = sum(num);
     System.out.println(total2);
+
+    List<Number> nums = List.of(29f, 44, 5f, 8f);
+    double total3 = sum(nums);
+    System.out.println(total3);
   }
 }
