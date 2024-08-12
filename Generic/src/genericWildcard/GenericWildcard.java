be566ed1732b953c03465d22e4e725a3e1451a72
plong rotha <Mrr.rothabetta31@gmail.com>
2024-08-12 14:45:59 +0700

    added comment

--- Generic/src/genericWildcard/GenericWildcard.java
+++ Generic/src/genericWildcard/GenericWildcard.java
@@ -7,6 +7,12 @@ public class GenericWildcard {
  * GenericWildCard can
  * -parameter
  * - field
- * -
+ * - local variable
+ * - return type
  * 
+ * 
+ * 
+ * Can not
+ * - involking a generic method
+ * - instanting a generic class
  */
