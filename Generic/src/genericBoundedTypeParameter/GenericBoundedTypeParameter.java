e5213b1227eda8ff9e00da5db6d692a7d8331a29
plong rotha <Mrr.rothabetta31@gmail.com>
2024-08-14 08:35:25 +0700

    update

--- Generic/src/genericBoundedTypeParameter/GenericBoundedTypeParameter.java
+++ Generic/src/genericBoundedTypeParameter/GenericBoundedTypeParameter.java
@@ -4,25 +4,25 @@ import java.util.List;
 
 public class GenericBoundedTypeParameter {
 
-  public static <T extends Number> double sum(List<T> numbers) {
-    double totle = 0;
-    for (T num : numbers) {
-      totle += num.doubleValue();
-    }
-    return totle;
-  }
-  
-  public static void main(String[] args) {
-    List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
-    double total = sum(numbers);
-    System.out.println(total);
+	public static <T extends Number> double sum(List<T> numbers) {
+		double totle = 0;
+		for (T num : numbers) {
+			totle += num.doubleValue();
+		}
+		return totle;
+	}
 
-    List<Long> num = List.of(29l, 44l, 562l, 678l);
-    double total2 = sum(num);
-    System.out.println(total2);
+	public static void main(String[] args) {
+		List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
+		double total = sum(numbers);
+		System.out.println(total);
 
-    List<Number> nums = List.of(29f, 44, 5f, 8f);
-    double total3 = sum(nums);
-    System.out.println(total3);
-  }
+		List<Long> num = List.of(29l, 44l, 562l, 678l);
+		double total2 = sum(num);
+		System.out.println(total2);
+
+		List<Number> nums = List.of(29f, 44, 5f, 8f);
+		double total3 = sum(nums);
+		System.out.println(total3);
+	}
 }
