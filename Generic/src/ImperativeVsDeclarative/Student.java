a1f3287e14b3d1e508a25a092ed95337550911b3
plong rotha <Mrr.rothabetta31@gmail.com>
2024-08-19 15:39:47 +0700

    add and test function

--- Generic/src/ImperativeVsDeclarative/Student.java
+++ Generic/src/ImperativeVsDeclarative/Student.java
@@ -4,14 +4,14 @@ public class Student {
 
 	private int id;
 	private String name;
-	private char gender;
+	private String gender;
 	private int grade;
 
 	public Student() {
 		super();
 	}
 
-	public Student(int id, String name, char gender, int grade) {
+	public Student(int id, String name, String gender, int grade) {
 		super();
 		this.id = id;
 		this.name = name;
@@ -35,11 +35,11 @@ public class Student {
 		this.name = name;
 	}
 
-	public char getGender() {
+	public String getGender() {
 		return gender;
 	}
 
-	public void setGender(char gender) {
+	public void setGender(String gender) {
 		this.gender = gender;
 	}
 
--- Generic/src/ImperativeVsDeclarative/StudentAppliction.java
+++ Generic/src/ImperativeVsDeclarative/StudentAppliction.java
@@ -2,6 +2,7 @@ package ImperativeVsDeclarative;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StudentAppliction {
 
@@ -10,14 +11,23 @@ public class StudentAppliction {
 
 	public static List<Student> students = List.of(
 
-			new Student(1, "Rotha", 'M', 12), new Student(2, "Leak", 'F', 10), new Student(3, "Pin", 'F', 11),
-			new Student(4, "Leap", 'F', 10), new Student(5, "Ti", 'M', 23), new Student(6, "Kan", 'M', 7),
-			new Student(7, "Sey", 'F', 11));
+			new Student(1, "Rotha", "M", 12), new Student(2, "Leak", "F", 10), new Student(3, "Pin", "F", 11),
+			new Student(4, "Leap", "F", 10), new Student(5, "Ti", "M", 23), new Student(6, "Kan", "M", 7),
+			new Student(7, "Sey", "F", 11)
+
+	);
 
 	public static void main(String[] args) {
 
+		System.out.println("imperative style");
 		List<Student> list1 = getstudents(students);
 		for (Student st : list1) {
+
+			System.out.println(st);
+		}
+		System.out.println("declarative style");
+		List<Student> list2 = getstudents(students);
+		for (Student st : list2) {
 
 			System.out.println(st);
 		}
@@ -31,7 +41,7 @@ public class StudentAppliction {
 		int counter = 0;
 		int limit = 3;
 		for (Student st : students) {
-			if (st.getGender() == 'F') {
+			if (st.getGender() == "F") {
 				list.add(st);
 				counter++;
 			}
@@ -46,7 +56,8 @@ public class StudentAppliction {
 	// declarative style
 	public static List<Student> getstudentsDeclarative(List<Student> students) {
 
-		return null;
+		return students.stream().filter(st -> st.getGender().equals("F")).limit(3).collect(Collectors.toList());
+
 	}
 
 }
