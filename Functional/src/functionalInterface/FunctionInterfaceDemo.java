ef78fc6488be132462d6fc5a5246b2ee460f24ea
plong rotha <Mrr.rothabetta31@gmail.com>
2024-08-23 10:30:08 +0700

    added predicateInterface

--- Functional/src/functionalInterface/FunctionInterfaceDemo.java
+++ Functional/src/functionalInterface/FunctionInterfaceDemo.java
@@ -8,6 +8,18 @@ public class FunctionInterfaceDemo {
 		Function<String, Integer> getlength = new Tolength();
 		Integer length = getlength.apply("Rotha");
 		System.out.println(length);
+
+		// =================================
+
+		Function<String, Integer> getlenght2 = name -> name.length();
+		System.out.println(getlenght2.apply("PlongRotha"));
+		System.out.println(getlenght2.apply("MynameIsRotha"));
+
+		Function<String, Character> getchar = text -> text.charAt(0);
+		System.out.println(getchar.apply("Sey"));
+
+		Function<Integer, Integer> getpower = n -> (n * n) / 2;
+		System.out.println(getpower.apply(9));
 	}
 
 	static class Tolength implements Function<String, Integer> {
@@ -16,7 +28,6 @@ public class FunctionInterfaceDemo {
 		public Integer apply(String t) {
 			return t.length();
 		}
-
 	}
 
 }
---
+++ Functional/src/functionalInterface/PredicateInterface.java
@@ -0,0 +1,10 @@
+package functionalInterface;
+
+public class PredicateInterface {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
