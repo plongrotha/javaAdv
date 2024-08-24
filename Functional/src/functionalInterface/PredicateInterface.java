a80bc8e1e59446db10798dca346a8c1d2ea9b165
plong rotha <Mrr.rothabetta31@gmail.com>
2024-08-24 09:33:28 +0700

    added predicate

--- Functional/src/functionalInterface/PredicateInterface.java
+++ Functional/src/functionalInterface/PredicateInterface.java
@@ -1,10 +1,15 @@
 package functionalInterface;
 
+import java.util.function.Predicate;
+
 public class PredicateInterface {
 
 	public static void main(String[] args) {
 		
-
+	
+		Predicate<String> containA = text -> text.toLowerCase().contains("A");
+		boolean A = containA.test("Rotha");
+		System.out.println(A);
 	}
-
+  
 }
