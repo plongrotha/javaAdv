290fd74ad30dfe275c12d54b917a3f863bc65feb
plong rotha <Mrr.rothabetta31@gmail.com>
2024-09-03 10:58:11 +0700

    added supplier interface

--- Functional/src/functionalInterface/PredicateInterface.java
+++ Functional/src/functionalInterface/PredicateInterface.java
@@ -7,10 +7,22 @@ public class PredicateInterface {
 	public static void main(String[] args) {
 		
 	
-		Predicate<String> containA = text -> text.contains("A");
-		Predicate<String> containIgnore = text -> text.toUpperCase().contains("A");
-		boolean A = containIgnore.test("Rotha");
-		System.out.println(A);
+//		Predicate<String> containA = text -> text.contains("A");
+//		Predicate<String> containIgnore = text -> text.toUpperCase().contains("A");
+//		boolean A = containIgnore.test("Rotha");
+//		System.out.println(A);
+		
+		Predicate<Integer> greaterthan = num -> num > 10;
+		
+
+		boolean test1 = greaterthan.test(20);
+		boolean test2 = greaterthan.test(2);
+		
+		System.out.println(test1);
+		System.out.println(test2);
+		
+		
+		
 	}
 	
   
--- Functional/src/functionalInterface/SupllierDemo.java
+++ Functional/src/functionalInterface/SupllierDemo.java
@@ -1,8 +1,19 @@
 package functionalInterface;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Supplier;
+
 public class SupllierDemo {
 
 	public static void main(String[] args) {
+
+		Supplier<String> str = () -> "Hello world!";
+		System.out.println(str.get());
 		
+		
+
 	}
 }
+
+// supplier is an interface
