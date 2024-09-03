0c456ff56ea690b412b125692ac44403c980db68
plong rotha <Mrr.rothabetta31@gmail.com>
2024-09-03 14:40:32 +0700

    Excersice

--- Functional/src/functionalInterface/SupllierDemo.java
+++ Functional/src/functionalInterface/SupllierDemo.java
@@ -8,12 +8,27 @@ public class SupllierDemo {
 
 	public static void main(String[] args) {
 
-		Supplier<String> str = () -> "Hello world!";
-		System.out.println(str.get());
+		/*
+		 * Supplier<String> str = () -> "Hello world!"; System.out.println(str.get());
+		 * 
+		 */
+
+		List<Integer> list = List.of(11, 43, 31, 43, 52, 75, 77);
+		Optional<Integer> firstEven = list.stream().filter(n -> n % 2 == 0).findFirst();
+		System.out.println(firstEven);
+		if (firstEven.isPresent()) {
+			System.out.println(firstEven.get() + 10);
+		}
+
+		List<String> names = List.of("man", "girl", "dog", "cat", "you");
+		names.stream().forEach(name -> System.out.println(name));
 		
-		
+		names.stream().map(s -> s.toUpperCase()).forEach(name -> System.out.println(name));
+		names.stream().map(s -> s.length()).forEach(name -> System.out.println(name));
 
 	}
 }
 
 // supplier is an interface
+
+// when error we can think to optional we must do condition to stop it
